a279a3c740e0be0525ece0a2c18f8dbee915ffbe
Diego Fernandes <fernandes.profissional@hotmail.com>
2022-10-01 23:49:30 -0300

    ajustes iniciais rotas | swagger | primeiros testes

---
+++ ProEvento/ProEventoApi/Controllers/EventoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.Api.Models;
+using System;
+
+namespace ProEvento.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoController : ControllerBase
+    {
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento()
+            {
+                EventoId = 1,
+                Tema = ".NET Core 5 + Angular",
+                Local = "Campinas - SP",
+                Lote = "1° lote",
+                QuantidadePessoas = 500,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+
+            };
+        }
+
+    }
+}
